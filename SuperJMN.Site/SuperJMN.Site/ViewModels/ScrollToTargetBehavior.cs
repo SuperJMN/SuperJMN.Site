1413827c9357594f0f9a0c740075a9db77f36d5f
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2024-11-25 22:22:03 +0100

    Add ScrollToTarget

---
+++ SuperJMN.Site/SuperJMN.Site/ViewModels/ScrollToTargetBehavior.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+using Avalonia.Xaml.Interactions.Custom;
+using CSharpFunctionalExtensions;
+using Zafiro.Avalonia.Mixins;
+
+namespace SuperJMN.Site.ViewModels;
+
+public class ScrollToTargetBehavior : AttachedToVisualTreeBehavior<InputElement>
+{
+    public static readonly AttachedProperty<string> TargetIdProperty =
+        AvaloniaProperty.RegisterAttached<ScrollToTargetBehavior, AvaloniaObject, string>("TargetId");
+
+    public static void SetTargetId(AvaloniaObject obj, string value) => obj.SetValue(TargetIdProperty, value);
+    public static string GetTargetId(AvaloniaObject obj) => obj.GetValue(TargetIdProperty);
+
+    public string TargetId { get; set; }
+
+    protected override void OnAttachedToVisualTree(CompositeDisposable disposable)
+    {
+        if (AssociatedObject is null)
+        {
+            return;
+        }
+
+        AssociatedObject.OnEvent(InputElement.PointerPressedEvent, RoutingStrategies.Tunnel)
+            .Do(_ =>
+            {
+                GetScrollViewer(AssociatedObject)
+                    .Execute(scrollViewer => GetTarget(scrollViewer)
+                        .Execute(visual => ScrollToTarget(scrollViewer, visual)));
+            })
+            .Subscribe()
+            .DisposeWith(disposable);
+
+        base.OnAttached(disposable);
+    }
+
+    private Maybe<ScrollViewer> GetScrollViewer(Visual visual)
+    {
+        return visual.GetVisualAncestors().OfType<ScrollViewer>().TryFirst();
+    }
+
+    private Maybe<Visual> GetTarget(Visual root)
+    {
+        return root.GetVisualDescendants().TryFirst(visual => GetTargetId(visual) == TargetId);
+    }
+
+    private void ScrollToTarget(ScrollViewer scrollViewer, Visual target)
+    {
+        var targetBounds = target.Bounds;
+        var transform = target.TransformToVisual(scrollViewer);
+
+        if (transform == null)
+        {
+            return;
+        }
+
+        var targetRectInScrollViewer = transform.Value.Transform(targetBounds.TopLeft);
+        scrollViewer.Offset = new Vector(scrollViewer.Offset.X, targetRectInScrollViewer.Y + scrollViewer.Offset.Y);
+    }
+}
