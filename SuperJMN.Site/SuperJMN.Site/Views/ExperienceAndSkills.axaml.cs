3b830267ec0e1efd27b1858966d5dda018193866
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2024-11-26 13:23:18 +0100

    Push later

---
+++ SuperJMN.Site/SuperJMN.Site/Views/ExperienceAndSkills.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SuperJMN.Site.Views;
+
+public partial class ExperienceAndSkills : UserControl
+{
+    public ExperienceAndSkills()
+    {
+        InitializeComponent();
+    }
+}
